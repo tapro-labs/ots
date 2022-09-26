f52d3ab1a4cb5d09a6487d23707bdbcf6609f74d
KaloyanYosifov <kaloqn665@gmail.com>
2022-09-26 22:58:30 +0200

    Set ip addess to all

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,15 +1,17 @@
 use std::fmt::{Display, Formatter};
+use std::net::Ipv4Addr;
 
 use redis::RedisResult;
-use rocket::{get, post, Request, Response, routes};
+use rocket::config::Config;
 use rocket::fairing::{Fairing, Info, Kind};
 use rocket::http::Header;
 use rocket::response::status::BadRequest;
-use rocket::serde::{Deserialize, json::Json, Serialize, Serializer};
 use rocket::serde::ser::SerializeStruct;
+use rocket::serde::{json::Json, Deserialize, Serialize, Serializer};
+use rocket::{get, post, routes, Request, Response};
+use uuid::Uuid;
 
 use store::redis_store::RedisStore;
-use uuid::Uuid;
 
 use crate::utils::time::Time;
 
@@ -18,94 +20,101 @@ mod utils;
 
 #[get("/")]
 fn index() -> &'static str {
-  "Hello, world!"
+    "Hello, world!"
 }
 
 #[derive(Deserialize)]
 struct CreateSecretData<'r> {
-  pub secret: &'r str
+    pub secret: &'r str,
 }
 
 struct CreateSecretResponse {
-  id: CustomUUID
+    id: CustomUUID,
 }
 
 impl CreateSecretResponse {
-  pub fn new(id: CustomUUID) -> Self {
-    Self { id }
-  }
+    pub fn new(id: CustomUUID) -> Self {
+        Self { id }
+    }
 }
 
 impl Serialize for CreateSecretResponse {
-  fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error> where S: Serializer {
-    let mut state = serializer.serialize_struct("SecretResponse", 1)?;
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut state = serializer.serialize_struct("SecretResponse", 1)?;
 
-    state.serialize_field("secretId", &self.id.to_string())?;
+        state.serialize_field("secretId", &self.id.to_string())?;
 
-    state.end()
-  }
+        state.end()
+    }
 }
 
 struct GetSecretResponse {
-  secret: String
+    secret: String,
 }
 
 impl GetSecretResponse {
-  pub fn new(secret: String) -> Self {
-    Self { secret }
-  }
+    pub fn new(secret: String) -> Self {
+        Self { secret }
+    }
 }
 
 impl Serialize for GetSecretResponse {
-  fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error> where S: Serializer {
-    let mut state = serializer.serialize_struct("SecretResponse", 1)?;
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut state = serializer.serialize_struct("SecretResponse", 1)?;
 
-    state.serialize_field("secret", &self.secret.to_string())?;
+        state.serialize_field("secret", &self.secret.to_string())?;
 
-    state.end()
-  }
+        state.end()
+    }
 }
 
 struct CustomUUID {
-  uuid: String
+    uuid: String,
 }
 
 impl CustomUUID {
-  pub fn new_v4() -> Self {
-    return Self { uuid: Uuid::new_v4().to_string() }
-  }
+    pub fn new_v4() -> Self {
+        return Self {
+            uuid: Uuid::new_v4().to_string(),
+        };
+    }
 }
 
 impl Display for CustomUUID {
-  fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-    write!(f, "{}", self.uuid.to_string())
-  }
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.uuid.to_string())
+    }
 }
 
 #[post("/secrets", format = "json", data = "<data>")]
-fn store_secret(data: Json<CreateSecretData<'_>>) -> Result<Json<CreateSecretResponse>, BadRequest<String>> {
-  let mut redis_store = RedisStore::connect_default();
-  let id = CustomUUID::new_v4();
+fn store_secret(
+    data: Json<CreateSecretData<'_>>,
+) -> Result<Json<CreateSecretResponse>, BadRequest<String>> {
+    let mut redis_store = RedisStore::connect_default();
+    let id = CustomUUID::new_v4();
 
-  match redis_store.store_for_a_period(
-    format!("secret-{}", id),
-    data.secret,
-    Time::from_days(1)
-  ) {
-    Ok(_) => Ok(Json(CreateSecretResponse::new(id))),
-    _ => Err(BadRequest(None))
-  }
+    match redis_store.store_for_a_period(format!("secret-{}", id), data.secret, Time::from_days(1))
+    {
+        Ok(_) => Ok(Json(CreateSecretResponse::new(id))),
+        _ => Err(BadRequest(None)),
+    }
 }
 
 #[get("/secrets/<secret>")]
 fn get_secret(secret: String) -> Option<Json<GetSecretResponse>> {
-  let mut redis_store = RedisStore::connect_default();
-  let key = format!("secret-{}", secret);
+    let mut redis_store = RedisStore::connect_default();
+    let key = format!("secret-{}", secret);
 
-  match redis_store.get_and_forget(key) {
-    Ok(value) => Some(Json(GetSecretResponse::new(value))),
-    _ => None
-  }
+    match redis_store.get_and_forget(key) {
+        Ok(value) => Some(Json(GetSecretResponse::new(value))),
+        _ => None,
+    }
 }
 
 pub struct CORS;
@@ -115,28 +124,32 @@ impl Fairing for CORS {
     fn info(&self) -> Info {
         Info {
             name: "Attaching CORS headers to responses",
-            kind: Kind::Response
+            kind: Kind::Response,
         }
     }
 
     async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
         response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
-        response.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET, PATCH, OPTIONS"));
+        response.set_header(Header::new(
+            "Access-Control-Allow-Methods",
+            "POST, GET, PATCH, OPTIONS",
+        ));
         response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
     }
 }
 
 #[rocket::options("/<_..>")]
-fn all_options() {
-}
+fn all_options() {}
 
 #[rocket::main]
 async fn main() {
+    let mut config = Config::default();
+    config.address = Ipv4Addr::new(0, 0, 0, 0).into();
 
-  let _ = rocket::build()
-    .attach(CORS)
-    .mount("/", routes![index])
-    .mount("/api", routes![all_options, get_secret, store_secret])
-    .launch()
-    .await;
+    let _ = rocket::custom(config)
+        .attach(CORS)
+        .mount("/", routes![index])
+        .mount("/api", routes![all_options, get_secret, store_secret])
+        .launch()
+        .await;
 }
