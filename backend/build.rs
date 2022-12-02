abfd1a95b8780d882b9ed0f52af3d288fcf587f0
KaloyanYosifov <kaloqn665@gmail.com>
2022-12-02 12:42:08 +0100

    Inject tls only when we have the files

--- backend/build.rs
+++ backend/build.rs
@@ -1,3 +1,8 @@
+extern crate core;
+
+use std::env;
+use std::path;
+
 use dotenvy::{dotenv, dotenv_iter};
 
 fn main() {
@@ -6,7 +11,32 @@ fn main() {
 
         for env_var in dotenv_iter().unwrap() {
             let (key, value) = env_var.unwrap();
-            println!("cargo:rustc-env={key}={value}");
+            env::set_var(&key, &value);
+            println!("cargo:rustc-env={}={}", &key, &value);
         }
     }
+
+    if let Ok(profile) = env::var("ROCKET_PROFILE") {
+        if profile != "debug" {
+            return;
+        }
+
+        init_tls();
+    }
+}
+
+fn init_tls() {
+    let certs_path = path::Path::new("../server_ssl/default.crt");
+    let key_path = path::Path::new("../server_ssl/default.key");
+
+    if !certs_path.exists() || !key_path.exists() {
+        println!("Private key and certificate were not found. These are required tls connection. See server-ssl/README.md");
+        return;
+    }
+
+    println!(
+        "cargo:rustc-env=ROCKET_TLS={{certs=\"{}\",key=\"{}\"}}",
+        certs_path.to_str().unwrap(),
+        &key_path.to_str().unwrap()
+    );
 }
