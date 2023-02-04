8087b4b08923c520ae0e98acff7a2c28e9d95e9a
KaloyanYosifov <kaloqn665@gmail.com>
2023-02-05 00:16:47 +0100

    Add github link, so users can check out the project

--- frontend/src/components/TheHeader/TheHeader.vue
+++ frontend/src/components/TheHeader/TheHeader.vue
@@ -2,9 +2,17 @@
   <div
     class="sticky top-0 z-30 flex h-16 w-full justify-center bg-opacity-90 backdrop-blur transition-all duration-100 bg-base-100 text-base-content"
   >
-    <div class="bg-accent-content navbar bg-base-100">
+    <div class="flex justify-between bg-accent-content navbar bg-base-100">
       <a class="btn btn-ghost normal-case text-xl" href="/">
         <img alt="Logo" src="@assets/images/logo.png" />
+      </a>
+
+      <a
+        class="mr-4 text-white transition-opacity hover:opacity-60"
+        href="https://github.com/tapro-labs/ots"
+        target="_blank"
+      >
+        <github-icon />
       </a>
     </div>
   </div>
@@ -15,8 +23,10 @@
  * External dependencies.
  */
 import { defineComponent } from 'vue';
+import GithubIcon from '@/components/icons/GithubIcon/GithubIcon.vue';
 
 export default defineComponent({
   name: 'TheHeader',
+  components: { GithubIcon },
 });
 </script>
