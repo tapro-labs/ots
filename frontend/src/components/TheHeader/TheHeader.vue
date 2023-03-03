9bff6d6022d4f90e4c4fda1215e41c82751419e4
KaloyanYosifov <kaloqn665@gmail.com>
2023-03-03 10:34:11 +0100

    Update the header class

--- frontend/src/components/TheHeader/TheHeader.vue
+++ frontend/src/components/TheHeader/TheHeader.vue
@@ -2,7 +2,7 @@
   <div
     class="sticky top-0 z-30 flex h-16 w-full justify-center bg-opacity-90 backdrop-blur transition-all duration-100 bg-base-100 text-base-content"
   >
-    <div class="flex justify-between bg-accent-content navbar bg-base-100">
+    <div class="flex justify-between bg-accent-content navbar">
       <a class="btn btn-ghost normal-case text-xl" href="/">
         <img alt="Logo" src="@assets/images/logo.png" />
       </a>
