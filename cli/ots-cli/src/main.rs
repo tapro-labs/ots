3343012f6683ec0bcf678cc92d504653f50510b7
KaloyanYosifov <kyosifovs@proton.me>
2023-11-05 16:38:52 +0100

    #13 - Use clap for creating the cli handler

--- cli/ots-cli/src/main.rs
+++ cli/ots-cli/src/main.rs
@@ -1,3 +1,36 @@
+use clap::{Arg, Command};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = Command::new("One Time secret CLI")
+        .version("1.0")
+        .author("Kaloyan Yosifov<kaloqn665@gmail.com>")
+        .about("The CLI for fetching/creating secrets with ots!")
+        .arg_required_else_help(true)
+        .subcommand(
+            Command::new("create").about("Create a secret").arg(
+                Arg::new("text")
+                    .value_name("text")
+                    .required(true)
+                    .help("The text for the creation of the secret"),
+            ),
+        )
+        .subcommand(
+            Command::new("get").about("Fetch secret").arg(
+                Arg::new("id")
+                    .value_name("id")
+                    .required(true)
+                    .help("The id of the secret to fetch"),
+            ),
+        )
+        .get_matches();
+
+    match matches.subcommand() {
+        Some(("create", m)) => {
+            println!("Creating secret! {:?}", m);
+        }
+        Some(("get", m)) => {
+            println!("Fetching secret! {:?}", m);
+        }
+        _ => {}
+    }
 }
